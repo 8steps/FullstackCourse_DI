7cdd49d88c83aad9de4405d3428d76a1e500b788
8steps <45315250+8steps@users.noreply.github.com>
2026-06-16 16:20:32 +0300

    relevant message

---
+++ DI_Bootcamp/Week1/Day2/ExercisesXP/Exercise.py
@@ -0,0 +1,49 @@
+#Exercise_1
+print("Hello world\n"*4)
+#Exercise_2
+print((99**3)*8)
+#Exercise_3
+#>>> 5 < 3 #False
+#>>> 3 == 3 #True
+#>>> 3 == "3" #False
+#>>> "3" > 3 #False
+#>>> "Hello" == "hello" #False
+#Exercise_4
+computer_brand = "Gigabyte"
+
+print(f"I have a {computer_brand} computer.")
+#Exercise_5
+name = "Daniel"
+age = 30
+shoe_size = 43
+info = f"My name is {name} and my age is {str(age)} and my shoe size is {str(shoe_size)}"
+
+print(info)
+#Exercise_6
+a = 1
+b = 3
+if a > b :
+    print("Hello world")
+else :
+    print("Goodbye")    
+#Exercise_7
+Check_num = int(input("Lets check your number:"))
+
+if Check_num % 2 == 0:
+    print("Your number is even!")
+else:
+    print("Your number is odd :(")    
+#Exercise_8
+Name_Test = input("Lets check if our names match :").capitalize()
+
+if Name_Test == "Daniel":
+    print("Wow! our names match , here's a Thumbsup 👍")
+else :
+    print("Too bad our names dont match , here's a Thumbsdown 👎")    
+#Exercise_9
+Check_height = int(input("What's your heigh is in CM:"))
+
+if Check_height > 145:
+    print("You are tall enough to get on the roller coaster!")
+else:
+    print("I'm sorry you need to grow abit before riding the roller coaster")   
---
+++ DI_Bootcamp/Week1/Day2/ExercisesXPGold/Exercise2.py
@@ -0,0 +1,11 @@
+Month = int(input("Please type a month so we could check its season:"))
+
+if 3<= Month <=5:
+    print("Ohh look it's Spring!")
+elif 6<= Month <=8:
+    print("Well it's Summer!")
+elif 9<= Month <=11:
+    print("It's Going to be Autumn at this month!")
+else:
+    print("It's the cold Winter this time !")
+    
