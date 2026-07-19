aa4a0d49f08dd10fd4fb5948341bf9ebf2b4672e
8steps <45315250+8steps@users.noreply.github.com>
2026-07-19 18:33:54 +0300

    relevant message

--- DI_Bootcamp/Week2/Day3/Random_Sum.py
+++ DI_Bootcamp/Week2/Day3/Random_Sum.py
@@ -3,7 +3,9 @@ import random
 list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
 
 target_number = 3728
+
 i = 0
+
 for num in list_of_numbers:
     test_num = target_number - num
     if test_num in list_of_numbers:
